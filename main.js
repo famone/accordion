2e4b57b1baa7c539547198e0787776d4d7d5fe50
luxilla8 <34896641+luxilla8@users.noreply.github.com>
2018-02-16 16:33:02 -0500

    main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+var accordion = document.getElementsByClassName(".accordion-control");
+
+for (i=0; i<accordion.length; i++){
+  accordion[i].onclick = function() {
+    this.classList.toggle('active');
+    var panel = document.getElementsByClassName(".accordion-panel");
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight= null;
+    } else {
+      panel.style.maxHeight=panel.scrollHeight+"px";
+    }
+  }
+}
